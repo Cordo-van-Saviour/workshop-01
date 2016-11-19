a799fcec4616f64ecbad76d5581a3e7adddec54d
Uros Radovanovic <radovanovic.uros92@gmail.com>
2016-11-19 17:31:00 +0100

    added translation

--- src/main.js
+++ src/main.js
@@ -58,14 +58,17 @@ $(function () {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var color =  d3.scaleOrdinal(d3.schemeCategory20);
-        color.domain(STAGES);
+    var color =  d3.scaleOrdinal(d3.schemeCategory20);
+    color.domain(STAGES);
 
-        var y = d3.scaleLinear().range([height, 0]);
-        var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+    var x = d3.scaleLinear().range([0, width]);
+    var xBrush = d3.scaleLinear().range([0, width]);
 
-        var xAxis = d3.axisBottom(x);
-        var yAxis = d3.axisLeft(y);
+    var xAxis = d3.axisBottom(x);
+    var yAxis = d3.axisLeft(y);
+
+    var xBrushAxis = d3.axisBottom(xBrush);
 
     // GO GO GO :)
     d3.tsv("../tcga-cases.tsv", function(error, data) {
@@ -77,58 +80,88 @@ $(function () {
       y.domain([0, d3.max(data, function (d) {
         return +d.case_age_at_diagnosis;
       })]);
+      xBrush.domain([d3.min(data, function (d) { return +d.case_days_to_death; }), d3.max(data, function (d) { return +d.case_days_to_death; })]);
+
 
       svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-      .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Sepal Width (cm)");
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("Sepal Width (cm)");
+      /**
+     * Brush
+     */
+
+
+    var brush = d3.select(".scatter-plot svg");
+
+    height = height + 120;
+
+    brush.append("g")
+        .attr("class", "axis axis--grid")
+        .attr("transform", "translate("+ margin.left +"," + height + ")")
+        .call(d3.axisBottom(xBrush)
+            .ticks(DEFAULTS.x_tick_count));
+
+    brush.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate("+ margin.left +"," + height + ")")
+        .call(d3.axisBottom(xBrush));
+
+    brush.append("g")
+        .attr("transform", "translate("+ margin.left +"," + height + ")")
+        .attr("class", "brush")
+        .call(d3.brushX()
+            .extent([[0, -50], [width, 0]])
+            .on("end", brushended));
+
 
       svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Sepal Length (cm)")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Sepal Length (cm)")
 
       svg.selectAll(".dot")
-      .data(data)
-      .enter().append("path")
-      .attr("d", DOT_SHAPE)
-      .attr("transform", function(d) { return "translate(" + x(d.case_days_to_death) + "," + y(d.case_age_at_diagnosis) + ")"; })
-      .style("fill", "none")
-      .style("stroke", function(d) {
-        return color(d.case_pathologic_stage);
+        .data(data)
+        .enter().append("path")
+        .attr("class", "dot")
+        .attr("d", DOT_SHAPE)
+        .attr("transform", function(d) { return "translate(" + x(d.case_days_to_death) + "," + y(d.case_age_at_diagnosis) + ")"; })
+        .style("fill", "none")
+        .style("stroke", function(d) {
+          return color(d.case_pathologic_stage);
       });
 
-      svg.append("g")
-      .attr("class", "brush")
-      .call(d3.brushX()
-          .extent([[0, 0], [width, height]])
-          .on("end", brushended));
 
       function brushended() {
         if (!d3.event.sourceEvent) return; // Only transition after input.
         if (!d3.event.selection) return; // Ignore empty selections.
-        var d0 = d3.event.selection.map(x.invert),
-            d1 = d0.map(d3.timeDay.round);
 
-        // If empty when rounded, use floor & ceil instead.
-        if (d1[0] >= d1[1]) {
-          d1[0] = d3.timeDay.floor(d0[0]);
-          d1[1] = d3.timeDay.offset(d1[0]);
-        }
+        var selection = d3.event.selection;
+        x.domain(selection.map(xBrush.invert, xBrush));
+
+        console.log("working....");
+
+
+        svg.selectAll(".dot")
+          .attr("transform", function (d) {
+            var x1 = x(d.case_days_to_death),
+                y1 = y(d.case_age_at_diagnosis);
+
+            return "translate(" + x1 + "," + y1 + ")"
+          })
       }
-
 
       // var legend = svg.selectAll(".legend-wrapper")
       // .data(d3color.domain())
